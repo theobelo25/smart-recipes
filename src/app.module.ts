abbe3774f77388c404d5c4e14f62146585d1d609
Theodore Belo <139794007+StellarGrade-Theo@users.noreply.github.com>
2026-02-07 17:04:53 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { UsersModule } from './domain/users/users.module';
+
+@Module({
+  imports: [UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
